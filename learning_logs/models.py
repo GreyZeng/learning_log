bf4ee9f7714deef5a76a4a42e5b6c5162e8657d9
GreyZeng <410486047@qq.com>
2023-11-06 16:09:58 +0800

    update code

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,7 +7,7 @@ class Topic(models.Model):
     """用户学习的主题"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         """返回模型的字符串表示"""
         return self.text
@@ -18,7 +18,8 @@ class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
     class Meta:
         verbose_name_plural = 'entries'
 
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render, redirect
 
 from .forms import TopicForm, EntryForm
@@ -13,7 +14,8 @@ def index(request):
 
 @login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    """显示所有主题"""
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -22,6 +24,9 @@ def topics(request):
 def topic(request, topic_id):
     """显示单个主题及其所有条目"""
     topic = Topic.objects.get(id=topic_id)
+    # 确认请求属于当前用户
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -37,7 +42,9 @@ def new_topic(request):
         # POST提交数据，对数据进行处理
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     # 显示空表单或表单数据无效
     context = {'form': form}
@@ -67,8 +74,11 @@ def new_entry(request, topic_id):
 
 @login_required
 def edit_entry(request, entry_id):
+    """编辑已有条目"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     if request.method != 'POST':
         # 初次请求，用当前的条目填充表单
         form = EntryForm(instance=entry)
