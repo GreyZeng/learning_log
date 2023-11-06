733e5e6b34a93fb662ea854501b0d25cf92e69f8
GreyZeng <410486047@qq.com>
2023-11-06 15:37:08 +0800

    update code

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -17,7 +18,7 @@ class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
-
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     class Meta:
         verbose_name_plural = 'entries'
 
