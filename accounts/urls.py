06b813f7f849477ae5e0c8d539fd354fb666980d
GreyZeng <410486047@qq.com>
2023-11-06 11:55:11 +0800

    update code

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 
+from . import views
+
 app_name = 'accounts'
 
 urlpatterns = [
     # 包含默认的省份验证URL
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register')
 ]
